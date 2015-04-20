7186a3bffdf6cd13557803940bb25aa2c14ba640
Alex Merritt <merritt.alex@gmail.com>
2015-04-20 13:29:00 -0400

    modifications to mmap interposer

--- src/sys/ld/mmap.c
+++ src/sys/ld/mmap.c
@@ -14,11 +14,15 @@ static int ncolors = 0; // disabled
 __attribute__((constructor))
 void getcolors(void)
 {
-	const char *str = getenv("PGCOLOR");
+	const char *str = getenv("COLORS");
 	if (str)
 		ncolors = strtol(str, NULL, 10);
 	printf("--> using %d colors\n", ncolors);
 }
+
+// if you want to printf or other functionality,
+// surround with enable=0/enable=1
+static int enable = 1;
 
 void * mmap(void *addr, size_t len, int prot, int flags, int fd, off_t offset)
 {
@@ -31,14 +35,23 @@ void * mmap(void *addr, size_t len, int prot, int flags, int fd, off_t offset)
 		exit(EXIT_FAILURE);
 
 	rv = func(addr, len, prot, flags, fd, offset);
+	if (!enable)
+		return rv;
+
 	if (rv == MAP_FAILED)
 		return rv;
+	enable = 0; printf("--> !MAP_FAILED\n"); enable = 1;
 	if (prot & PROT_NONE)
 		return rv;
 	if ((prot & MAP_ANON) == 0)
 		return rv;
 	if ((prot & MAP_PRIVATE) == 0)
 		return rv;
+
+
+	enable = 0;
+	printf("--> MCONTROL\n");
+	enable = 1;
 
 	m = mcontrol(rv, len, MADV_PGCOLOR, ncolors);
 	if (m)
