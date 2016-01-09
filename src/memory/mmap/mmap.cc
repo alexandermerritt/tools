19962c5b5e5e908e1aef109547830dbd0b18ebe4
Alexander Merritt <merritt.alex@gmail.com>
2016-01-09 13:28:41 -0800

    mmap parallel / file use

--- src/memory/mmap/mmap.cc
+++ src/memory/mmap/mmap.cc
@@ -7,6 +7,7 @@
 #endif
 
 #include <iostream>
+#include <thread>
 
 #include <fcntl.h>
 #include <sched.h>
@@ -26,6 +27,32 @@ int pin(int cpu)
     return sched_setaffinity(getpid(), sizeof(mask), &mask);
 }
 
+struct page
+{
+    union {
+        char fluff[1<<12];
+        long value;
+    } u;
+};
+
+void touch(struct page *pages, off64_t start, size_t npages)
+{
+    printf("thread start %lu npages %lu\n", start, npages);
+    for (size_t pg = start; pg < (start+npages); pg++)
+        pages[pg].u.value = 0xdead + pg;
+}
+
+void parallel_touch(struct page *pages, size_t npages)
+{
+    int nthreads = 1;
+    size_t pages_per = (npages / nthreads);
+    thread **threads = new thread*[nthreads];
+    for (int t = 0; t < nthreads; t++)
+        threads[t] = new thread(touch, pages, (t*pages_per), pages_per);
+    for (int t = 0; t < nthreads; t++)
+        threads[t]->join();
+}
+
 int dommap(size_t len, size_t nreg, size_t niter,
         unsigned int flags, unsigned int perms)
 {
@@ -35,17 +62,28 @@ int dommap(size_t len, size_t nreg, size_t niter,
     if (pin(0))
         return 1;
 
+    printf("# attempting %lu bytes\n", len);
     size_t next = 0;
-    void **addrs = new void* [len];
-    if (!addrs)
-        return 1;
+    //void **addrs = new void* [len];
+    //if (!addrs)
+        //return 1;
 
+    len = (254254UL << 20);
+
+    struct page *pages = (struct page*)mmap(NULL, len, perms, flags, -1, 0);
+    if (pages == MAP_FAILED)
+        return 1;
+    //parallel_touch(pages, (len >> 20));
+    //for (size_t pg = 0; pg < (len >> 20); pg++)
+        //pages[pg].u.value = 0xdead + pg;
+
+    return 0;
     printf("total_ns len nregions niter r w pop nores\n");
     for (unsigned int i = 0; i < niter; i++) {
         clock_gettime(CLOCK_REALTIME, &t1);
         for (unsigned int i = 0; i < nreg; i++)
-            if ((addrs[next++] = mmap(NULL, len, perms, flags, -1, 0))
-                    == MAP_FAILED)
+            //if ((addrs[next++] = mmap(NULL, len, perms, flags, -1, 0))
+            if ((mmap(NULL, len, perms, flags, -1, 0)) == MAP_FAILED)
                 return 1;
         clock_gettime(CLOCK_REALTIME, &t2);
         diff = (t2.tv_sec * 1e9 + t2.tv_nsec) -
@@ -57,10 +95,12 @@ int dommap(size_t len, size_t nreg, size_t niter,
                 !!(PROT_WRITE & perms),
                 !!(MAP_POPULATE & flags),
                 !!(MAP_NORESERVE & flags));
-        for (unsigned int pg = 0; pg < nreg; pg++)
-            munmap(addrs[--next], len);
+        //for (unsigned int pg = 0; pg < nreg; pg++)
+            //munmap(addrs[--next], len);
     }
 
+    printf("sleeping..\n");
+    sleep(3600);
     return 0;
 }
 
@@ -69,12 +109,13 @@ int runmmap(void)
     unsigned int flags, perms;
     size_t len, nreg, niter;
 
-    len   = (1UL << 20);
-    nreg  = (1UL << 12);
+    len   = (1UL << 38);
+    //nreg  = (1UL << 12);
+    nreg  = 1;
     niter = 1;
 
-    flags  = MAP_PRIVATE | MAP_ANONYMOUS;
-    flags |= MAP_POPULATE;
+    flags  = MAP_PRIVATE | MAP_ANONYMOUS | MAP_HUGETLB;
+    //flags |= MAP_POPULATE;
     //flags |= MAP_UNINITIALIZED;
     flags |= MAP_NORESERVE;
 
@@ -105,9 +146,9 @@ size_t * dorunmmapfs(const char *fname, off64_t len, unsigned int perms, unsigne
     for (int i = 0; i < niter; i++) {
         clock_gettime(CLOCK_REALTIME, &t1);
         addr = mmap(nullptr, len, perms, flags, fd, 0);
+        clock_gettime(CLOCK_REALTIME, &t2);
         if (MAP_FAILED == addr)
             return nullptr;
-        clock_gettime(CLOCK_REALTIME, &t2);
 
         diff = (t2.tv_sec * 1e9 + t2.tv_nsec) -
             (t1.tv_sec * 1e9 + t1.tv_nsec);
@@ -122,25 +163,13 @@ size_t * dorunmmapfs(const char *fname, off64_t len, unsigned int perms, unsigne
 
 // Create a file on disk first, large enough to be mapped (e.g. 32G)
 //      dd if=/dev/zero of=/tmp/map_me bs=1M count=$((1<<15))
-int runmmapfs(void)
+int runmmapfs(unsigned int perms, unsigned int flags,
+        off64_t start, off64_t end, int niter)
 {
-    unsigned int perms = 0;
-    unsigned int flags = 0;
     size_t *times(nullptr);
     const char fname[] = "/tmp/map_me";
 
-    perms = PROT_READ | PROT_WRITE;
-    //perms = PROT_READ;
-    //perms = PROT_WRITE;
-    //perms = PROT_NONE;
-
-    //flags |= MAP_PRIVATE;
-    flags |= MAP_SHARED;
-    flags |= MAP_POPULATE;
     flags |= MAP_NORESERVE;
-
-    int niter = 16;
-    const off64_t start = (1<<30), end = (1UL<<32);
 
     for (off64_t len = start; len <= end; len <<= 1) {
         if (!(times = dorunmmapfs(fname, len, perms, flags, niter)))
@@ -155,12 +184,45 @@ int runmmapfs(void)
     return 0;
 }
 
-int main(void)
+int main(int argc, char *argv[])
 {
-    //if (runmmap())
-        //return 1;
+    if (argc < 2)
+        return 1;
 
-    if (runmmapfs())
+    string cmd(argv[1]);
+    if ("anon" == cmd) {
+        if (runmmap())
+            return 1;
+    } else if ("file" == cmd) {
+        unsigned int perms = 0;
+        unsigned int flags = 0;
+        int niter = 1;
+        off64_t start = 0, end = 0;
+        for (int i = 2; i < argc; i++) {
+            string arg(argv[i]);
+            if ("r" == arg)
+                perms |= PROT_READ;
+            else if ("w" == arg)
+                perms |= PROT_WRITE;
+            else if ("shared" == arg)
+                flags |= MAP_SHARED;
+            else if ("private" == arg)
+                flags |= MAP_PRIVATE;
+            else if ("pop" == arg)
+                flags |= MAP_POPULATE;
+            else if ('S' == arg[0])
+                start = 1UL<<strtol(&arg[1], nullptr, 10);
+            else if ('E' == arg[0])
+                end = 1UL<<strtol(&arg[1], nullptr, 10);
+            else if ('I' == arg[0])
+                niter = strtol(&arg[1], nullptr, 10);
+            else
+                return 1;
+        }
+
+        if (runmmapfs(perms, flags, start, end, niter))
+            return 1;
+    } else
         return 1;
 
     return 0;
